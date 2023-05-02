14b34b178dff112f590a11029927f71e3b95bcb6
OVEGERO <rat4355@gmail.com>
2023-05-02 16:03:08 -0500

    first commit

---
+++ myapp/logic/modeloSNN.py
@@ -0,0 +1,39 @@
+from keras.models import load_model
+import pickle
+import os
+from keras.utils import pad_sequences
+
+class modeloSNN():
+
+    def cargarRNN(self, nombreArchivo):
+        ruta = 'D:\\Universidad\\Aprendizaje Automatico\\sentimientos-env\\resources'
+        nombre_archivo = nombreArchivo + '.h5'
+        ruta_archivo = os.path.join(ruta, nombre_archivo)
+        modelo = load_model(ruta_archivo)
+        print("Red Neuronal Cargada desde Archivo") 
+        return modelo
+
+    def cargarTokenizer(self, nombreArchivo):
+        ruta = 'D:\\Universidad\\Aprendizaje Automatico\\sentimientos-env\\resources'
+        nombre_archivo = nombreArchivo + '.pickle'
+        ruta_archivo = os.path.join(ruta, nombre_archivo)
+        with open(ruta_archivo, 'rb') as handle:
+            tokenizer = pickle.load(handle)
+            return tokenizer
+        
+    def predecirNuevasMuestras(self, Comentario):
+        data = [Comentario]
+        dic = {0: 'Mala', 1: 'Bueno', 2: 'Excelente'}
+        self.tokenizer = self.cargarTokenizer(self,'tokenizer')
+        self.model = self.cargarRNN(self,'modelo_base')
+        X = self.tokenizer.texts_to_sequences(data)
+        X = pad_sequences(X, maxlen=166)
+        pred = self.model.predict(X)[0]
+        resp = ''
+        if pred.argmax(axis=0) == 0:
+            resp = (f'La calificación del restaurante es: {dic[pred.argmax(axis=0)]}. Con una certeza del {round(pred[pred.argmax(axis=0)]*100, 4)}%')
+        elif pred.argmax(axis=0) == 1:
+            resp = (f'La calificación del restaurante es: {dic[pred.argmax(axis=0)]}. Con una certeza del {round(pred[pred.argmax(axis=0)]*100, 4)}%')
+        else:
+            resp = (f'La calificación del restaurante es: {dic[pred.argmax(axis=0)]}. Con una certeza del {round(pred[pred.argmax(axis=0)]*100, 4)}%')
+        return resp 
---
+++ myapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .logic import modeloSNN
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+import json
+from django.http import JsonResponse
+
+# Create your views here.
+
+def determinarSentimiento(request):
+    return render(request, 'sentimientos.html')
+
+@api_view(['GET', 'POST'])
+def predecir(request):
+    try:
+        comentario = request.POST['comentario']
+        resul = modeloSNN.modeloSNN.predecirNuevasMuestras(modeloSNN.modeloSNN,Comentario=comentario)
+        print('resultado de la prediccion: ' + resul)
+        return render(request, 'informe.html', {
+            'resul': resul
+        })
+    except Exception as e:
+        return render(request, 'informe.html', {
+            'resul': e
+        })
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def predecirIOJson(request):
+    print(request)
+    print('****')
+    print(request.body)
+    print('****')
+    body = json.loads(request.body.decode('utf-8'))
+    # Formato de datos de entrada
+    comentario = body['comentario']
+    resul = modeloSNN.modeloSNN.predecirNuevasMuestras(modeloSNN.modeloSNN,Comentario=comentario)
+    data = {'resul': resul}
+    resp = JsonResponse(data)
+    resp['Access-Control-Allow-Origin'] = '*'
+    return resp
